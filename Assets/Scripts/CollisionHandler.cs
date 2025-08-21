400e9b6ff39169c8b39f05631075f29e3f78573f
kimsehun0530 <kimsehun0530@gmail.com>
2025-08-21 21:21:51 +0900

    2일차

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "S":
+                Debug.Log("시작합니다");
+                break;
+
+             case "Finish":
+                Debug.Log("도착");
+                break;
+
+             case "F":
+                Debug.Log("연료를 얻었다!");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
